8fee8ea745d1e9160c1dddfea198ab455d7314d4
Kalyani Singh <kanu.atoz@gmail.com>
2018-03-17 16:05:08 -0400

    location change

---
+++ App/javascripts/ChirpApp.js
@@ -0,0 +1,17 @@
+var app = angular.module('ChirpApp', []);
+
+app.controller("mainController", function($scope){
+    $scope.posts = [];
+    $scope.newPost ={created_by : '',
+                         text : '',
+                         created_at : ''};
+
+    $scope.post = function(){
+        $scope.newPost.created_at = Date.now();
+        $scope.posts.push($scope.newPost);
+        $scope.newPost = {created_by:'', text:'', created_at:''};
+
+    };
+
+    
+});
