a8b7fe83f8e7ca508f784585cb22cfd0ad4d0b35
Kalyani Singh <kanu.atoz@gmail.com>
2018-03-17 15:44:03 -0400

    added variables to controller

--- App/ChirpApp.js
+++ App/ChirpApp.js
@@ -1,8 +1,9 @@
 var app = angular.module('ChirpApp', []);
 
 app.controller("mainController", function($scope){
-
-
-
+    var $scope.posts = [];
+    var $scope.newPost ={created_by : "",
+                         text : "",
+                         created_at : ""};
     
 });
