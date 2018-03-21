cbcf2509e4bfb64b1fab5b37c990f6823bb62b08
Kalyani Singh <kanu.atoz@gmail.com>
2018-03-20 21:34:35 -0400

    change routes and add crud functionality

--- Node/app.js
+++ Node/app.js
@@ -4,7 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var passport = require('passport');
+var session = require('express-session');
 var api = require('./routes/api');
 //var authentication = require('./routes/authentication');
 
@@ -17,11 +18,15 @@ app.set('view engine', 'ejs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(session({
+    secret: 'super duper secret'
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(passport.initialize());
+app.use(passport.session());
 app.use('/api', api);
 //app.use('/auth', authenticate);
 
--- Node/routes/api.js
+++ Node/routes/api.js
@@ -1,4 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+router.route('/posts')
+
+    .get(function(req, res){
+    res.send({message: 'TODO return all posts'});
+    })  //return all posts
+
+    .post(function(req, res){
+        res.send({message: 'TODO: Cerate new post'});
+    });
+
+router.route('/posts/:id')
+
+        .get(function (req, res){
+            res.send({message : 'TODO return post with ID' +req.params.id});
+        })
+
+        .put(function(req,res){
+            res.send({message : 'TODO modify post with ID' +req.params.id});
+        })
+
+        .delete(function(req,res){
+            res.send({message : 'TODO delete post with ID' +req.params.id});
+        });
+
 module.exports = router;
